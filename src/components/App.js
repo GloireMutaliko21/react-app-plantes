1296224e709e577cc124502e1ca488ee234cd4ec
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-06-30 14:03:20 +0200

    feat: using of <children> prop for Banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Banner from './Banner'
-import Cart from './Cart'
+// import Cart from './Cart'
 import ShoppingList from './ShoppingList';
+import "../styles/Banner.css"
+import logo from '../assets/logo.png'
 
 function App() {
+  const title = "La maison jungle"
   return (<div>
-    <Banner />
+    <Banner>
+      <img src={logo} alt='La maison jungle' className="lmj-logo" />
+      <h1 className="lmj-title">La maison jungle</h1>
+    </Banner>
     {/* <Cart /> */}
     <ShoppingList />
   </div>)
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,16 +2,9 @@ import React from "react";
 import "../styles/Banner.css"
 import logo from '../assets/logo.png'
 
-function Banner() {
+function Banner({children}) {
     const title = "La maison jungle"
-    return (
-        <div>
-            <div className='lmj-banner'>
-                <img src={logo} alt='La maison jungle' className="lmj-logo" />
-                <h1 className="lmj-title">{title}</h1>
-            </div>
-        </div>)
-
+    return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
