b1b35966e1dc7703f7b24b2d47e9696ad2e6d49a
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-02 06:07:24 +0200

    feat: state open/close cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,8 +6,11 @@ function Cart() {
     const [cart, updateCart] = useState(0)
     const monsteraPrice = 8
 
-    return (
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
         <div className="lmj-cart">
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
             <div>
                 Monstera : {monsteraPrice}€
@@ -17,6 +20,8 @@ function Cart() {
             </div>
             <h3>Total : {monsteraPrice * cart}€</h3>
         </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
     )
 }
 
