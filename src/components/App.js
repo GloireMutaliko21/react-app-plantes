2ceb8ebbf9e9f164980bfdd192a6a76d5f3c0347
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-02 05:42:38 +0200

    feat: adding initial state to Cart component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from './Banner'
-// import Cart from './Cart'
+import Cart from './Cart'
 import ShoppingList from './ShoppingList';
 import logo from '../assets/logo.png'
 import Footer from './Footer';
@@ -12,7 +12,7 @@ function App() {
         <img src={logo} alt='La maison jungle' className="lmj-logo" />
         <h1 className="lmj-title">La maison jungle</h1>
       </Banner>
-      {/* <Cart /> */}
+      <Cart />
       <ShoppingList />
       <Footer />
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,21 @@
 import React from "react";
 import '../styles/Cart.css'
+import { useState } from "react";
 
 function Cart() {
+    const [cart, updateCart] = useState(0)
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
+
     return (
         <div className="lmj-cart">
             <h2>Panier</h2>
-            <ul>
-                <li>Monstera : {monsteraPrice}€</li>
-                <li>Lierre : {ivyPrice}€</li>
-                <li>Fleurs : {flowerPrice}€</li>
-            </ul>
-            Total : {monsteraPrice + ivyPrice + flowerPrice}€
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
         </div>
     )
 }
